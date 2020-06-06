72cc68b697a85d39b991f1c8620a168d92889416
ss8u19 <ss8u19@soton.ac.uk>
2020-06-06 21:36:40 +0100

    updated content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainContent(){
     return(
-        <p className="MainContent"> first year computer science student at the university of southampton</p>
+        <p className="MainContent">first year computer science student at the university of southampton
+        <br/>from romania
+        </p>
     )
 }
 
